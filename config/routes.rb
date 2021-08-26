67ac851cdce224a96c389e016ba9a0b3152c381c
suzuya0 <sakura240613@gmail.com>
2021-08-26 03:27:05 +0000

    [Add]topview作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  
+  root to:'homes#top'
+  get 'homes/about' => 'homes#about'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  
+  
+  resources :users,only:[:index,:show,:edit,:update]
+
+  
 end
