eabc1c7613063f9addfc85f46ef83714a8afa84d
suzuya0 <sakura240613@gmail.com>
2021-09-03 03:33:57 +0000

    [Fix]フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,8 @@ Rails.application.routes.draw do
   resources :users,only:[:index,:show,:edit,:update] do
     resource :relationships,only: [:create,:destroy]
     get 'followings' => 'relationships#followings', as:'followings'
-    get 'followers' => 'relationships#followers', as:'followwers'
+    get 'followers' => 'relationships#followers', as:'followers'
   end
-  
-  
   
 end
 
