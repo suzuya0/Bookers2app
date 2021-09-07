8867fb4fde7c571a557dd2da9f11e6295a16cfe6
suzuya0 <sakura240613@gmail.com>
2021-09-07 12:28:02 +0000

    [Fix]コメント、いいね機能の非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,21 @@
 class BookCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    render :index
+ 
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+  	book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
+    render :index
   end
+  
   
   private
   
